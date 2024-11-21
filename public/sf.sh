626f2bc7a878c3738df1fe50b082f7181c50a8a5
holzi1005 <andreas.holzmann@h2-invent.com>
2024-11-21 09:38:28 +0100

    fix typo

--- public/sf.sh
+++ public/sf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # export SIMPLE_FILEMANAGER_AUTH_TOKEN=0314e4dd5031799beb52a24e6521e38204c75be01f64794c96a9af11ec47e61a
-# export SIMPLE_FILEMANGER_DOMAIN=http://localhost:8080
+# export SIMPLE_FILEMANAGER_DOMAIN=http://localhost:8080
 cat << "EOF"
     ___  _  _                                                          
   / __\(_)| |  ___  _ __ ___    __ _  _ __    __ _   __ _   ___  _ __ 
@@ -13,7 +13,7 @@ by H2 invent
 
 EOF
 
-echo "The file will be uploaded to: $SIMPLE_FILEMANGER_DOMAIN"
+echo "The file will be uploaded to: $SIMPLE_FILEMANAGER_DOMAIN"
 echo ""
 
 read -p "Enter the TITLE: " SIMPLE_FILEMANAGER_TITLE
@@ -22,7 +22,7 @@ read -p "Expire after X hours: " SIMPLE_FILEMANAGER_EXPIRE
 read -p "Enter EMAIL of user for permission: " SIMPLE_FILEMANAGER_EMAIL
 read -p "Enter the FILE PATH: " SIMPLE_FILEMANAGER_FILE
 
-curl -X POST $SIMPLE_FILEMANGER_DOMAIN/filemanager/upload \
+curl -X POST $SIMPLE_FILEMANAGER_DOMAIN/filemanager/upload \
      -H "Authorization: Bearer $SIMPLE_FILEMANAGER_AUTH_TOKEN" \
      -F "title=$SIMPLE_FILEMANAGER_TITLE" \
      -F "description=$SIMPLE_FILEMANAGER_DESCRIPTION" \
